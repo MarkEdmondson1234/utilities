08a0408969334ea66d118edf3bdc84e0f3ac3859
Mark <github@markedmondson.me>
2016-02-23 23:54:56 +0100

    add error funcs

--- utilities.R
+++ utilities.R
@@ -1,3 +1,28 @@
+#' Is this a try error?
+#' 
+#' Utility to test errors
+#' 
+#' @param test_me an object created with try()
+#' 
+#' @return Boolean
+#' 
+#' @keywords internal
+is.error <- function(test_me){
+  inherits(test_me, "try-error")
+}
+
+#' Get the error message
+#'
+#' @param test_me an object that has failed is.error
+#'
+#' @return The error message
+#'
+#' @keywords internal
+error.message <- function(test_me){
+  if(is.error(test_me)) attr(test_me, "condition")$message
+}
+
+
 #' A helper function that tests whether an object is either NULL _or_
 #' a list of NULLs
 #'
